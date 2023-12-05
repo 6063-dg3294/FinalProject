606f797e01e41f6d12664e389dd633a57132d282
Difei (Amber) Gao <dg3294@nyu.edu>
2023-12-05 14:19:27 -0500

    1

--- sketch.js
+++ sketch.js
@@ -1,6 +1,23 @@
+let mSerial;
+
+function connect(){
+  mSerial.open(9600);
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  mSerial = createSerial();
+   
+  let mConnectButton = createButton("Connect to Serial");
+  mConnectButton.position(width/2, height/2);
+  mConnectButton.mousePressed(connect);
 }
 
 function draw() {
+  background(0);
+  if (mSerial.opened()&& mSerial.availableBytes() > 0){
+    let mLine = mSerial.readUntil("\n");
+    print(mline);
+  }
+
 }
